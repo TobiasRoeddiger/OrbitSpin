3e3873d3aec75788114ba6867199d0b683e3e976
Tobias Roeddiger <wasserglas@t-online.de>
2017-07-07 17:14:26 +0100

    Update example.cs

--- example.cs
+++ example.cs
@@ -1,6 +1,6 @@
 public class OrbitSpin {
 
-	private Stopwatch _stopwatch;
+    private Stopwatch _stopwatch;
 	
     private const int RotationTime = 2500; //in ms
     private const double _ballRadius = 20.0;
@@ -8,29 +8,29 @@ public class OrbitSpin {
     private const double _centerX = 20.0;
     private const double _centerY = 20.0;
         
-	public OrbitSpin()
-	{
-		_stopwatch = new Stopwatch();
-    	_stopwatch.Start();
-    	        
-		var timer = new Timer();
-    	timer.Tick += (sender, args) => { MakeSpinStep(); };
-    	timer.Interval = 20; //call every 20 ms
-    	timer.Start();
-	}
+    public OrbitSpin()
+    {
+	_stopwatch = new Stopwatch();
+   	_stopwatch.Start();
+   
+	var timer = new Timer();
+  	timer.Tick += (sender, args) => { MakeSpinStep(); };
+ 	timer.Interval = 20; //call every 20 ms
+        timer.Start();
+    }
 
-	private void MakeSpinStep()
-	{
-	    var elapsedTime = _stopwatch.ElapsedMilliseconds;
+    private void MakeSpinStep()
+    {
+        var elapsedTime = _stopwatch.ElapsedMilliseconds;
 	
-	    var theta = Math.PI * 2f * elapsedTime / RotationTime;
+	var theta = Math.PI * 2f * elapsedTime / RotationTime;
 	
-		var ballCenterX = _centerX + _swingRadius * Math.Cos(theta);
-	  	var ballCenterY = _centerY + _swingRadius * Math.Sin(theta);
+        var ballCenterX = _centerX + _swingRadius * Math.Cos(theta);
+        var ballCenterY = _centerY + _swingRadius * Math.Sin(theta);
 	
-	  	var ballLeft = ballCenterX - _ballRadius;
-	  	var ballTop = ballCenterY - _ballRadius;
+        var ballLeft = ballCenterX - _ballRadius;
+        var ballTop = ballCenterY - _ballRadius;
 	
-	  	//TODO: set top and left of the spinning trajectory
+        //TODO: set top and left of the spinning trajectory
 	}
 }
